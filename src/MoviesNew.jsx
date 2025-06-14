8a757c0f6319c8a8b06a86b7bae329033a7ced7c
KelvinCastle13 <kelvincastle13@gmail.com>
2025-06-14 18:14:26 -0400

    Add new/create actions

---
+++ src/MoviesNew.jsx
@@ -0,0 +1,28 @@
+export function MoviesNew({onCreate}) {
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const params = new FormData(form);
+    const successCallback = () => form.reset();
+    onCreate(params, successCallback);
+  }
+  
+  return (
+    <div>
+      <h1>New Movie</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          Title: <input title="title" type="text" />
+        </div>
+        <div>
+          Year: <input year="year" type="text" />
+        </div>
+        <div>
+          Plot: <input plot="plot" type="text" />
+        </div>
+        <button type="submit">Create</button>
+      </form>
+    </div>
+  );
+}
--- src/MoviesPage.jsx
+++ src/MoviesPage.jsx
@@ -1,6 +1,7 @@
-import { MoviesIndex } from "./MoviesIndex"
+import { MoviesIndex } from "./MoviesIndex";
 import axios from "axios";
 import {useState, useEffect} from "react";
+import { MoviesNew } from "./MoviesNew";
 
 export function MoviesPage() {
   const [movies, setMovies] = useState([]);
@@ -12,10 +13,20 @@ export function MoviesPage() {
     });
   };
 
+  const handleCreate = (params, successCallback) => {
+    console.log("handleCreate");
+    axios.post("/movies.json", params).then((response) => {
+      setMovies([...movies, response.data]);
+      successCallback();
+    });
+  }
+
   useEffect(handleIndex, []);
   
   return (
     <main>
+      {/* <MoviesNew /> */}
+      <MoviesNew onCreate={handleCreate} />
       <MoviesIndex movies={movies} />
     </main>
   )
